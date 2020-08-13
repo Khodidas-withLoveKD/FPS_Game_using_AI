6758a9d7d248972721810ddc2ce59a26f48ff0ec
Khodidas_with_love_KD <41850210+Khodidas-withLoveKD@users.noreply.github.com>
2020-08-13 12:37:14 +0530

    Imported Guns and done little scripting (coding)

---
+++ FPS_Game_using_AI/Assets/Scripts/Player's Script/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController characterController;    //Our naming convention is different, we will use camelCase 
+
+    private Vector3 moveDirection;
+    public float speed = 5f;
+    private float gravity = 20f;
+    public float jumpForce = 10f;
+    private float verticalVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
